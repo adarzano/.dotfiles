b399fa33d8826a34b12e47ea8a898f013a4784d6
Darzano <adarzano@cocsce-l1d43-29.ds.sc.edu>
2023-04-05 17:37:27 -0400

    Created linux shell setup scripts.

--- bin/cleanup.sh
+++ bin/cleanup.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 # Remove the .vimrc file in the home directory
-rm ~/.vimrc
+rm -f ~/.vimrc
 
 # Remove the line 'surce ~/.dotfiles/etc/bashrc custom' in the .bashrc file
-sed -i '/source ~\/.dotfiles\/etc\/bashrc custom/d' ~/.bashrc
+# sed -i 's/beep/bop/g' ~/.bashrc
+sed -i 's/source \~\/\.dotfiles\/etc\/bashrc\_custom//g' ~/.bashrc
+#sed -i '/source ~\/.dotfiles\/etc\/bashrc custom/d' ~/.bashrc
 
 # Remove the .TRASH directory in the home directory
-rm -r ~/".TRASH"o
+rm -rf ~/.TRASH
